73e341340a0d6a89c1ad27737ab7aa91d08bbf87
Om Jain <omjain2606@gmail.com>
2020-12-25 23:44:40 +0530

    Integrated with backend

--- src/Pages/Login_SignUp/forms/login.js
+++ src/Pages/Login_SignUp/forms/login.js
@@ -4,20 +4,22 @@ class Login extends Component {
   state = {
     email: "",
     password: "",
+    error: "",
   };
 
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  onSubmit = (e) => {
+  onSubmit = async (e) => {
+    const { email, password } = this.state;
     e.preventDefault();
     const elements = document.getElementsByClassName("error");
     for (var i = elements.length - 1; i >= 0; i--) {
       elements[i].remove();
     }
     Object.keys(this.state).forEach((ele) => {
-      if (this.state[ele] === "") {
+      if (this.state[ele].trim() === "") {
         document.getElementsByName(ele).forEach((sub) => {
           sub.insertAdjacentHTML(
             "afterend",
@@ -26,6 +28,31 @@ class Login extends Component {
         });
       }
     });
+    if (email.trim() !== "" && password.trim() !== "") {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      var raw = JSON.stringify({ email, password });
+
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      try {
+        const res = await fetch(
+          "http://localhost:8080/user/login",
+          requestOptions
+        );
+        const data = await res.json();
+        if (data.error) {
+          this.setState({ error: data.error });
+        }
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
   };
 
   render() {
@@ -34,7 +61,7 @@ class Login extends Component {
         <h1>Sign in</h1>
         <input
           id="email"
-          type="email"
+          type="text"
           placeholder="Email"
           name="email"
           value={this.state.email}
@@ -53,6 +80,9 @@ class Login extends Component {
             Forgot your password?
           </a>
         </div>
+        {this.state.error && (
+          <div className="error main-error">{this.state.error}</div>
+        )}
         <button type="submit" id="signin-btn" className="btn">
           Sign In
         </button>
--- src/Pages/Login_SignUp/forms/register.js
+++ src/Pages/Login_SignUp/forms/register.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 class Register extends Component {
   state = {
-    firstName: "",
-    lastName: "",
-    contactNo: "",
+    fname: "",
+    lname: "",
+    contactNumber: "",
     year: "",
     college: "",
     email: "",
@@ -16,22 +16,83 @@ class Register extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  onSubmit = (e) => {
+  onSubmit = async (e) => {
     e.preventDefault();
     const elements = document.getElementsByClassName("error");
     for (var i = elements.length - 1; i >= 0; i--) {
       elements[i].remove();
     }
-    Object.keys(this.state).forEach((ele) => {
-      if (this.state[ele] === "") {
+    Object.keys(this.state).forEach(async (ele) => {
+      if (this.state[ele].trim() === "") {
         document.getElementsByName(ele).forEach((sub) => {
           sub.insertAdjacentHTML(
             "afterend",
-            `<div class="error"> *Required </div>`
+            `<div class="error">*Required</div>`
           );
         });
       }
     });
+
+    const {
+      fname,
+      lname,
+      year,
+      college,
+      contactNumber,
+      email,
+      password,
+    } = this.state;
+
+    if (password !== this.state.confirmPassword) {
+      document.getElementsByName("confirmPassword").forEach((sub) => {
+        sub.insertAdjacentHTML(
+          "afterend",
+          `<div class="error"> Password do not match </div>`
+        );
+      });
+    } else if (
+      Object.keys(this.state).every((ele) => this.state[ele].trim() !== "")
+    ) {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      var raw = JSON.stringify({
+        fname,
+        lname,
+        email,
+        password,
+        contactNumber,
+        college,
+        year,
+      });
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+      try {
+        const res = await fetch(
+          "http://localhost:8080/user/register",
+          requestOptions
+        );
+        const data = await res.json();
+        console.log(data.error);
+
+        if (data.error) {
+          const errors = data.error;
+          for (let i = 0; i < errors.length; i++) {
+            const key = Object.keys(errors[i])[0];
+            const val = Object.values(errors[i])[0];
+            document.getElementsByName(key).forEach((sub) => {
+              sub.insertAdjacentHTML(
+                "afterend",
+                `<div class="error"> ${val}</div>`
+              );
+            });
+          }
+        }
+      } catch (e) {}
+    }
   };
 
   render() {
@@ -40,23 +101,23 @@ class Register extends Component {
         <input
           type="text"
           placeholder="First Name"
-          name="firstName"
-          value={this.state.firstName}
+          name="fname"
+          value={this.state.fname}
           onChange={this.onChange}
         />
         <input
           type="text"
           placeholder="Last Name"
-          name="lastName"
-          value={this.state.lastName}
+          name="lname"
+          value={this.state.lname}
           onChange={this.onChange}
         />
 
         <input
           type="text"
           placeholder="Contact number"
-          name="contactNo"
-          value={this.state.contactNo}
+          name="contactNumber"
+          value={this.state.contactNumber}
           onChange={this.onChange}
         />
         <input
@@ -76,7 +137,7 @@ class Register extends Component {
         />
 
         <input
-          type="email"
+          type="text"
           placeholder="Email"
           name="email"
           value={this.state.email}
